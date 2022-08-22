a60b3f09b4ca644f15a181b0c75f8fb8e8ddc15c
204-bhanuteja <105474392+204-bhanuteja@users.noreply.github.com>
2022-08-22 22:09:32 +0530

    Add files via upload

---
+++ changeofstringpositions.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class swap
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(first_last("bhanu"));
+        Console.WriteLine(first_last("sonata"));
+        Console.WriteLine(first_last("golla"));
+    }
+    public static string first_last(string ustr)
+    {
+        return ustr.Length > 1
+            ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
+    }
+}
